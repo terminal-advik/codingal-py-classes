d39c7e0cd45fd9883a07bcb09c966f5affb6def9
Advik <advik.singhal@icloud.com>
2026-09-05 14:01:21 -0500

    1st commit

---
+++ Module 1/lesson 1/Activity1.py
@@ -0,0 +1,12 @@
+#This is my name
+print("Hello, this is Advik")
+
+
+#about my age
+print("I'm 11 years old")
+
+# About my hobbies
+print("My favorite hobby is Chess")
+
+#what is 3 + 5
+print("the sum of 3 + 5 is ",3+5)
