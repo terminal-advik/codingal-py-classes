4437a4f0b3162b528ef57d791eb0ac72ba8ca485
Advik <advik.singhal@icloud.com>
2026-09-06 13:32:38 -0500

    Co-authored-by: Sandeep Tripathy <sandeep-tripathy@users.noreply.github.com>

---
+++ Module 1/lesson2/Activity2.py
@@ -0,0 +1,38 @@
+#LECTURE 2
+import keyword
+#1---->How to  print multiple values inside 1 print function.
+
+# write your age.
+# write your roll number
+# what is your favorite hobby 
+
+# print(11,20,"basketball") 
+
+#2-->\n (NEW LINE CHARACTER)
+
+# print("Advik\nsinghal")
+
+#3-->Natural \n of print function
+
+# print("Advik") #Advik \n
+
+# print("Sandeep \nTripathy")
+# print("Advik")
+
+
+#4--->Identifier : 1.A------Z
+#                  2.a------z
+#                  3. 0----9
+#                  4. _
+# It can't start with a number(or digit) and no keywords are allowed.
+
+x = 10
+print(x)
+
+#5---> Variable : It is a Box with a label
+x = "Advik"
+print(x)
+
+#6--->Keywords: Reserve words in a programming language.
+
+print(len(keyword.kwlist))
