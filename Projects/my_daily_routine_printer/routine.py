8ce729dd53e48fc890d74ec439d4268239b0b386
Advik <advik.singhal@icloud.com>
2026-09-06 10:09:52 -0500

    Daily routine printer hw

---
+++ Projects/my_daily_routine_printer/routine.py
@@ -0,0 +1,15 @@
+# My Daily Routine Printer
+
+print("\n")
+print("*********************************************************************************************")
+print("                           MY DAILY ROUTINE!!! (for school: mon-fri) ") 
+print("*********************************************************************************************\n")
+print("1: Wake up at 6:20 P.M\n") 
+print("2: Eat breakfast\n") 
+print("3: Brush my teeth\n")
+print("4: Get ready for school( change clothes( if needed), comb hair, etc)\n")
+print("5: Use toilettrees (lotion, deodorant, etc)\n")
+print("6: Grab backpack and lunch box\n")
+print("7: Be ready for my carpoolers to come pick me up & have FUN AT SCHOOL!!!!!!!!!!!!!!!!!! \n") 
+print("My total study time per day is about 30 minutes\n ")
+print("Total study time in a week :", 0.5 * 7, "hours\n"  )
