d0423af189d5e783011a1e3a444da7b06bbd30cb
Advik <advik.singhal@icloud.com>
2026-09-20 13:45:57 -0500

    lesson4

---
+++ Module 1/lesson4/classroom.py
@@ -0,0 +1,79 @@
+#------------->OPERATORS<-----------------
+
+# # Operators: A symbol used to perform operation on values or variables.
+
+# a = 10
+# b = 20
+
+# print(a + b) # a,b ---> operands | '+' is operator | a + b is expression.
+
+# #------------->Arithmatic Operators<--------------
+# x = 10
+# y = 3
+
+# # '+' operator
+# print(x + y) #13
+
+# # '-' operator
+# print(x - y) #7
+
+# # '*' operator
+# print(x * y) #30
+
+# # '/' operator
+# print(x / y) #3.3333333
+
+# # '//' operator (floor division operator or integer division operator)
+# print(x // y) # 3
+
+# # Ceil Division formula : (a + b - 1) // b
+
+
+# # '%' operator
+# print(x % y) # 1 
+
+# # Power operator
+# print(x ** y) # x to the power y # 1000
+
+# #-------->COMPARISION OPERATORS<--------
+
+# x = 10
+# y = 5
+
+# #In comparision operator the result can either be True or False
+
+# print(x == y) # False
+
+# print(x != y) # True
+
+# print(x > y) # True
+
+# print(x < y) # False
+
+# y = 10
+# print(x >= y) # True
+
+# print(x <= y) # True
+
+#------>Assignment Operators<------------
+
+'=' 
+a = 10
+b = 12
+
+c = a + b
+
+print(a) #10
+
+a = a + 5
+
+print(a) #15
+
+a += 5 #Same as a = a + 5 
+print(a) #20
+
+a -= 5 #same as a = a - 5
+print(a) #15
+
+a *= 5 #75
+print(a) #75
